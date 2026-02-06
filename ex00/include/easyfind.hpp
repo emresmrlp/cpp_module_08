4bc2864b52fe5b0c8b56deae764084c5d7edf23b
Yunus Emre Can Sümeralp <emresmrlp@gmail.com>
2026-02-06 16:58:32 +0300

    feat(ex00): easyfind() template, find algorithm and main.cpp test file added

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,7 +6,21 @@
 /*   By: ysumeral <ysumeral@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:26:20 by ysumeral          #+#    #+#             */
-/*   Updated: 2026/02/05 14:42:53 by ysumeral         ###   ########.fr       */
+/*   Updated: 2026/02/06 16:57:48 by ysumeral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <algorithm>
+# include <iostream>
+
+template <typename T>
+typename T::iterator easyfind(T &type, int value);
+
+template <typename T>
+typename T::const_iterator easyfind(const T &type, int value);
+
+# include "easyfind.tpp"
+
+#endif
--- ex00/include/easyfind.tpp
+++ ex00/include/easyfind.tpp
@@ -6,7 +6,18 @@
 /*   By: ysumeral <ysumeral@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 14:26:19 by ysumeral          #+#    #+#             */
-/*   Updated: 2026/02/05 14:42:50 by ysumeral         ###   ########.fr       */
+/*   Updated: 2026/02/06 16:57:46 by ysumeral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template <typename T>
+typename T::iterator easyfind(T &type, int value)
+{
+    return (std::find(type.begin(), type.end(), value));
+}
+
+template <typename T>
+typename T::const_iterator easyfind(const T &type, int value)
+{
+    return (std::find(type.begin(), type.end(), value));
+}
